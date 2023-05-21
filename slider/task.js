69802a0bde1e97ef7b284052904c95fa62869174
Viola Pavlova <violacyrus96@Gmail.com>
2023-05-21 15:17:29 +0300

    small changes

--- slider/task.js
+++ slider/task.js
@@ -6,13 +6,13 @@ let sliderItems = document.querySelectorAll('.slider__item');
 
 function showActiveSlide(index) {
     let currIndex = [...sliderItems].findIndex(slider =>
-        slider.classList.contains('slider__item_active'))
+        slider.classList.contains('slider__item_active'));
     sliderItems[currIndex].classList.remove('slider__item_active');
     sliderDots[currIndex].classList.remove('slider__dot_active');
     if (index < 0) {
-        index = sliderItems.length - 1
+        index = sliderItems.length - 1;
     } else if (index > (sliderItems.length - 1)) {
-        index = 0
+        index = 0;
     }
     sliderItems[index].classList.add('slider__item_active');
     sliderDots[index].classList.add('slider__dot_active');
@@ -20,18 +20,18 @@ function showActiveSlide(index) {
 
 arrowNext.onclick = () => {
     currIndex = [...sliderItems].findIndex(slider =>
-        slider.classList.contains('slider__item_active'))
-    showActiveSlide(currIndex + 1)
+        slider.classList.contains('slider__item_active'));
+    showActiveSlide(currIndex + 1);
 }
 
 arrowPrev.onclick = () => {
     currIndex = [...sliderItems].findIndex(slider =>
-        slider.classList.contains('slider__item_active'))
-    showActiveSlide(currIndex - 1)
+        slider.classList.contains('slider__item_active'));
+    showActiveSlide(currIndex - 1);
 }
 
 sliderDots.forEach.call(sliderDots, (dot) => {
     dot.addEventListener('click', () => {
-        showActiveSlide([...sliderDots].indexOf(dot))
+        showActiveSlide([...sliderDots].indexOf(dot));
     })
 })
